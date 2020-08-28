b1a45f3eecb28e7e7872f1b13e27206a9b0885b1
chri715 <maigaard.christian@gmail.com>
2020-08-28 23:25:25 +0200

    clean-up unnessary bootstrapping from client and add pages structure

---
+++ client/src/Pages/PackagesPage/PackagesPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PackagesPage = (props) => {
+  return (
+    <ul>
+      <li>
+        <a href="#">Coffee</a>
+      </li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+  );
+};
+
+export default PackagesPage;
