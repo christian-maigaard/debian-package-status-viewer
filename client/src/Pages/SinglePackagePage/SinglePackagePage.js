82e76de52cbff6cdad2ac51a0e23473160fe5def
chri715 <maigaard.christian@gmail.com>
2020-08-29 16:15:03 +0200

    added package title, description and basic depends field

--- client/src/Pages/SinglePackagePage/SinglePackagePage.js
+++ client/src/Pages/SinglePackagePage/SinglePackagePage.js
@@ -1,9 +1,34 @@
 import React from "react";
+import Loading from "../../common/Loading";
+import { request } from "../../api/request";
+import envVariables from "../../envVariables";
 
 const SinglePackagePage = (props) => {
   const { packageId } = props;
 
-  return <h1>{packageId}</h1>;
+  const [debianPackage, setDebianPackage] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetch() {
+      const result = await request(envVariables.PACKAGES_ENDPOINT + packageId);
+      setDebianPackage(result);
+    }
+    fetch();
+  }, []);
+
+  return (
+    <div>
+      <h1>{debianPackage.Package}</h1>
+      <h2>Description</h2>
+      <p>{debianPackage.Description}</p>
+      <h2>Dependencies</h2>
+      {debianPackage.Depends !== undefined || debianPackage.Depends !== "" ? (
+        <p>{debianPackage.Package + " depends on " + debianPackage.Depends}</p>
+      ) : (
+        <p>{debianPackage.Package + " has no dependencies"}</p>
+      )}
+    </div>
+  );
 };
 
 export default SinglePackagePage;
--- client/src/envVariables.js
+++ client/src/envVariables.js
@@ -1,3 +1,2 @@
-const PACKAGES_ENDPOINT = "http://localhost:5000/packages";
-
+const PACKAGES_ENDPOINT = "http://localhost:5000/packages/"; 
 export default { PACKAGES_ENDPOINT };
