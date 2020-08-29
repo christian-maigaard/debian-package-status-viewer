8d75dbd374f254e6a10437da845c8c8f25e741cc
chri715 <maigaard.christian@gmail.com>
2020-08-29 16:44:22 +0200

    get package by id endpoint now returns package dependencies and reverse dependencies

--- server/controllers/packageController.js
+++ server/controllers/packageController.js
@@ -2,9 +2,41 @@ const packagesController = require("./PackagesController");
 
 const getPackage = (id, callback) => {
   packagesController.getAllPackages((packages) => {
-    const package = packages.find((p) => p.Package === id);
-    callback(package);
+    let package = packages.find((p) => p.Package === id);
+    package = addDependencies(package);
+    addReverseDependencies(package, packages, (enrichedPackage) => {
+      callback(enrichedPackage);
+    });
   });
+};
+
+// adds package dependencies and reverse dependencies to the package
+const addDependencies = (package) => {
+  package.Dependencies = {};
+  // Remove text between parentheses
+  const PackageDependencies = package.Depends.replace(/ *\([^)]*\) */g, "");
+  // format into list to make data ready for front-end
+  package.Dependencies.PackageDependencies = PackageDependencies.split(",").map(
+    (dependency) => {
+      return dependency.trim();
+    }
+  );
+  package.Dependencies.PackageDependenciesCount =
+    package.Dependencies.PackageDependencies.length;
+  return package;
+};
+
+const addReverseDependencies = (package, packages, callback) => {
+  // search all packages for dependencies matching the current package
+  let reverseDependencies = [];
+  for (p in packages) {
+    if (packages[p].Depends === undefined) continue;
+    if (packages[p].Depends.includes(package.Package))
+      reverseDependencies.push(packages[p].Package);
+  }
+
+  package.Dependencies.ReverseDependencies = reverseDependencies;
+  callback(package);
 };
 
 module.exports = { getPackage };
