d848416f375038a8099d2d4e614e9a0ebcf36c20
chri715 <maigaard.christian@gmail.com>
2020-08-29 20:10:31 +0200

    added react router to list items

--- client/src/common/atoms/ListItemLink.js
+++ client/src/common/atoms/ListItemLink.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ListItemLink = (props) => {
   const { text, link } = props;
   return (
     <li key={text + "-" + Math.floor(Math.random() * 10000)}>
-      <a href={link}>{text}</a>
+      <Link to={link}>{text}</Link>
     </li>
   );
 };
